4d6311fe6180ed580142159d21b0bc184b7a8d1c
Imdadul Hoq <hoq.imdadul@gmail.com>
2016-04-10 23:34:10 -0400

    implemented PrimeNumber

--- src/math/PrimeNumber.java
+++ src/math/PrimeNumber.java
@@ -1,5 +1,7 @@
 package math;
 
+import java.util.ArrayList;
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
@@ -9,7 +11,22 @@ public class PrimeNumber {
 		 * Out put number of Prime numbers on the given range.
 		 * 
 		 */
-		
+		for (int i= 2;i<100000;i++){
+			if(isPrime(i))
+				System.out.print(i + ", ");
+		}
+	}
+
+	public static boolean isPrime(int n) {
+		//check if n is a multiple of 2
+		if(n ==2) return  true;
+		if (n%2==0) return false;
+		//if not, then just check the odds
+		for(int i=3;i*i<=n;i+=2) {
+			if(n%i==0)
+				return false;
+		}
+		return true;
 	}
 
 }
